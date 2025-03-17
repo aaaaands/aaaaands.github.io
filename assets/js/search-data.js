cf34ec0013f50697894affc377c018382a5b2251
aaaaands <153921586+aaaaands@users.noreply.github.com>
2025-03-17 16:02:43 +0000

    Deploying to gh-pages from @ aaaaands/aaaaands.github.io@83af23b8b663baf7f324abd3f46b7f91a6b8d7a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum",
+          title: "Curriculum 📄",
+          description: "You can download my [CV in PDF](assets/pdf/Curriculum_.pdf).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
